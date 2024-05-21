0444547f1916c4f7a5af5ddf20d006410ac84739
qinchao <qinchao956@163.com>
2024-05-22 02:29:53 +0800

    log

--- source/common/log.cpp
+++ source/common/log.cpp
@@ -1,2 +1,65 @@
 #include "log.h"
+#include <cstdarg>
 
+namespace cassobee
+{
+
+std::string to_string(LOG_LEVEL level)
+{
+    switch(level)
+    {
+        case LOG_LEVEL_DEBUG: { return "DEBUG"; } break;
+        case LOG_LEVEL_INFO:  { return "INFO";  } break;
+        case LOG_LEVEL_WARN:  { return "WARN";  } break;
+        case LOG_LEVEL_ERROR: { return "ERROR"; } break;
+        case LOG_LEVEL_FATAL: { return "FATAL"; } break;
+        default:{ return "UNKNOWN"; } break;
+    }
+}
+
+log_formatter::log_formatter(const std::string pattern)
+    : _pattern(pattern)
+{
+}
+
+std::string log_formatter::format(LOG_LEVEL level, const std::string& content)
+{
+    
+}
+
+file_appender::file_appender(const char* logdir, const char* filename)
+    : _logdir(logdir), _filename(filename)
+{
+}
+
+void file_appender::log(LOG_LEVEL level, const std::string& content)
+{
+    
+}
+
+logger::logger()
+{
+    _root_appender = new file_appender("/home/cassobee/debug/logdir/", "log.h");
+}
+
+void logger::log(LOG_LEVEL level, const std::string& content)
+{
+    _root_appender->log(level, content);
+}
+
+void log_manager::init()
+{
+    _root_logger = new logger;
+}
+
+void log_manager::log(LOG_LEVEL level, const char* fmt, ...)
+{
+    char buf[2048];
+    va_list args;
+    va_start(args, fmt);
+    int n = vsnprintf(buf, sizeof(buf), fmt, args);
+    va_end(args);
+    _root_logger->log(level, std::string(buf, n));
+}
+
+}
--- source/common/log.h
+++ source/common/log.h
@@ -7,37 +7,52 @@
 namespace cassobee
 {
 
-class log_event
+enum LOG_LEVEL
 {
-public:
-
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_FATAL,
 };
+
+std::string to_string(LOG_LEVEL level);
 
 class log_formatter
 {
 public:
-    virtual void format();
+    log_formatter(const std::string pattern);
+    std::string format(LOG_LEVEL level, const std::string& content);
+    class formatter_item
+    {
+    public:
+        virtual void format(std::ostream& os) = 0;
+    };
+
+private:
+    std::string _pattern;
 };
 
 class log_appender
 {
 public:
-    virtual void log() = 0;
+    virtual void log(LOG_LEVEL level, const std::string& content) = 0;
     FORCE_INLINE void set_formatter(log_formatter* formatter) { _formatter = formatter; }
 private:
-    log_formatter* _formatter = nullptr;
+    log_formatter* _formatter;
 };
 
 class console_appender : public log_appender
 {
 public:
-    virtual void log() override;
+    virtual void log(LOG_LEVEL level, const std::string& content) override;
 };
 
 class file_appender : public log_appender
 {
 public:
-    virtual void log() override;
+    file_appender(const char* logdir, const char* filename);
+    virtual void log(LOG_LEVEL level, const std::string& content) override;
 private:
     std::string _logdir;
     std::string _filename;
@@ -46,16 +61,11 @@ private:
 class logger
 {
 public:
-    void init();
-    void log(const char* fmt, ...);
-    void debug(const char* fmt, ...);
-    void info(const char* fmt, ...);
-    void warn(const char* fmt, ...);
-    void error(const char* fmt, ...);
-    void fatal(const char* fmt, ...);
+    logger();
+    void log(LOG_LEVEL level, const std::string& content);
 
-    void add_appender(log_appender* appender);
-    void del_appender(log_appender* appender);
+    void add_appender(const std::string& name, log_appender* appender);
+    void del_appender(const std::string& name);
     void clr_appender();
 private:
     log_appender* _root_appender = nullptr;
@@ -65,7 +75,12 @@ private:
 class log_manager : public singleton_support<logger>
 {
 public:
+    void init();
+    void log(LOG_LEVEL level, const char* fmt, ...);
 
+    void get_logger(std::string name);
+    void add_logger(std::string name, logger* logger);
+    void del_logger(std::string name);
 private:
     logger* _root_logger = nullptr;
     std::unordered_map<std::string, logger*> _loggers;
