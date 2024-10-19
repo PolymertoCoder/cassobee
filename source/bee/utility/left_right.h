77721a351e8becdecbf843a3b0a63d247ae09e6b
qinchao <qinchao956@163.com>
2024-10-20 02:48:24 +0800

    left right

---
+++ source/bee/utility/left_right.h
@@ -0,0 +1,160 @@
+#pragma once
+#include <atomic>
+#include <thread>
+
+#include "lock.h"
+
+// see cassobee/doc/CppCon/How to make your data structures wait-free for reads - Pedro Ramalhete - CppCon 2015.pdf
+
+enum LEFT_RIGHT
+{
+    LEFT  = 0,
+    RIGHT = 1,
+};
+
+class read_indicator
+{
+public:
+    virtual void arrive() = 0;   // mark the arrival(of a reader)
+    virtual void depart() = 0;   // mark the departure(of a reader)
+    virtual bool is_empty() = 0; // return false if there is a thread that has arrived but not yet departed
+};
+
+class ri_atomic_counter : public read_indicator
+{
+public:
+    virtual void arrive() override
+    {
+        _counter.fetch_add(1);
+    }
+    virtual void depart() override
+    {
+        _counter.fetch_sub(1);
+    }
+    virtual bool is_empty() override
+    {
+        return _counter.load() == 0;
+    }
+
+private:
+    std::atomic<uint64_t> _counter{0};
+};
+
+class ri_distributed_counter : public read_indicator
+{
+public:
+    ri_distributed_counter()
+    {
+        constexpr static int DEFAULT_COUNTER_NUMS = 8;
+        _counters_nums = std::thread::hardware_concurrency();
+        if(_counters_nums == 0)
+        {
+            _counters_nums = DEFAULT_COUNTER_NUMS;
+        }
+        _counters = new std::atomic<uint64_t>[_counters_nums * CACHE_LINE_WORDS];
+    }
+    ~ri_distributed_counter()
+    {
+        delete[] _counters;
+    }
+    virtual void arrive() override
+    {
+        _counters[thread_to_idx()].fetch_add(1);
+    }
+    virtual void depart() override
+    {
+        _counters[thread_to_idx()].fetch_sub(1);
+    }
+    virtual bool is_empty() override
+    {
+        int idx = _acquire_load.load();
+        for(; idx < _counters_nums * CACHE_LINE_WORDS; idx += CACHE_LINE_WORDS)
+        {
+            if(_counters[idx].load(std::memory_order_relaxed) > 0)
+            {
+                std::atomic_thread_fence(std::memory_order_acquire);
+                return false;
+            }
+        }
+        std::atomic_thread_fence(std::memory_order_acquire);
+        return true;
+    }
+
+private:
+    int thread_to_idx()
+    {
+        size_t idx = _hash_func(std::this_thread::get_id());
+        return (int)((idx % _counters_nums) * CACHE_LINE_WORDS);
+    }
+
+private:
+    int _counters_nums;
+    std::atomic<uint64_t>* _counters;
+    std::atomic<uint64_t>  _acquire_load{0};
+    std::hash<std::thread::id> _hash_func;
+};
+
+using lrc_t = ri_distributed_counter;
+
+template<typename T>
+class left_right
+{
+public:
+    template<typename Fn, typename ...Args>
+    requires std::invocable<Fn, Args...>
+    auto apply_read(Fn& func, Args&&... args)
+    {
+        const int vi = arrive();
+        T* inst = _left_right.load() == LEFT ? &_left_inst : &_right_inst;
+        auto ret = func(inst, std::forward<Args>(args)...);
+        depart(vi);
+        return ret;
+    }
+    template<typename Fn, typename ...Args>
+    requires std::invocable<Fn, Args...>
+    auto apply_write(Fn& func, Args&&... args)
+    {
+        const int vi = arrive();
+        T* inst = _left_right.load() == LEFT ? &_left_inst : &_right_inst;
+        auto ret = func(inst, std::forward<Args>(args)...);
+        return ret;
+    }
+
+private:
+    int arrive()
+    {
+        const int vi = _version_index.load();
+        _lrc[vi].arrive();
+        return vi;
+    }
+    void depart(int vi)
+    {
+        _lrc[vi].depart();
+    }
+    void toggle_version_and_wait()
+    {
+        const int vi  = _version_index.load();
+        const int pvi = (int)(vi & 0x1);
+        const int nvi = (int)((vi + 1) & 0x1);
+        // wait for readers from next version
+        while(!_lrc[pvi].is_empty())
+        {
+            std::this_thread::yield();
+        }
+        // toggle the version index variable
+        _version_index.store(nvi);
+        // wait for readers from previous version
+        while(!_lrc[nvi].is_empty())
+        {
+            std::this_thread::yield();
+        }
+    }
+
+private:
+    lrc_t _lrc[2];
+    std::atomic<int> _version_index = 0;
+    std::atomic<int> _left_right    = LEFT;
+    T _left_inst;
+    T _right_inst;
+};
+
