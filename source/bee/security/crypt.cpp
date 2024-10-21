4c26c3c65af8d287782bcf871006736fd82e80d3
qinchao <qinchao956@163.com>
2024-10-22 01:41:50 +0800

    libsecurity AES_crypt

--- source/bee/security/crypt.cpp
+++ source/bee/security/crypt.cpp
@@ -1,2 +1,55 @@
 #include "crypt.h"
+#include "log.h"
 
+#include "modes.h"
+#include "filters.h"
+
+using byte = unsigned char;
+
+std::string AES_crypt::encrypt(const std::string_view& plaintext)
+{
+    return encrypt(plaintext.data(), plaintext.size());
+}
+
+std::string AES_crypt::encrypt(const char* plaintext, size_t size)
+{
+    std::string ciphertext;
+    try
+    {
+        CryptoPP::CBC_Mode<CryptoPP::AES>::Encryption encryption;
+        encryption.SetKeyWithIV(_key, _key.size(), _iv);
+
+        CryptoPP::StringSource ss(reinterpret_cast<const byte*>(plaintext), size, true,
+            new CryptoPP::StreamTransformationFilter(encryption, new CryptoPP::StringSink(ciphertext)));
+    }
+    catch(const CryptoPP::Exception& e)
+    {
+        ERRORLOG("AES Encryption error: %s", e.what());
+        throw;
+    }
+    return ciphertext;
+}
+
+std::string AES_crypt::decrypt(const std::string_view& ciphertext)
+{
+    return decrypt(ciphertext.data(), ciphertext.size());
+}
+
+std::string AES_crypt::decrypt(const char* ciphertext, size_t size)
+{
+    std::string decryptedtext;
+    try
+    {
+        CryptoPP::CBC_Mode<CryptoPP::AES>::Decryption decryption;
+        decryption.SetKeyWithIV(_key, _key.size(), _iv);
+
+        CryptoPP::StringSource ss(reinterpret_cast<const byte*>(ciphertext), size, true,
+                new CryptoPP::StreamTransformationFilter(decryption, new CryptoPP::StringSink(decryptedtext)));
+    }
+    catch (const CryptoPP::Exception& e)
+    {
+        ERRORLOG("AES Decryption error: %s", e.what());
+        throw;
+    }
+    return decryptedtext;
+}
--- source/bee/security/crypt.h
+++ source/bee/security/crypt.h
@@ -1,19 +1,33 @@
 #pragma once
 #include <string>
 #include <string_view>
+#include "aes.h"
+#include "osrng.h"
+#include "secblockfwd.h"
 
 class crypt
 {
 public:
     crypt() = default;
     virtual ~crypt() = default;
-    virtual std::string encrypt(const std::string_view& input) = 0;
-    virtual std::string decrypt(const std::string_view& input) = 0;  
+    virtual std::string encrypt(const std::string_view& plaintext)  = 0;
+    virtual std::string encrypt(const char* plaintext, size_t size) = 0;
+
+    virtual std::string decrypt(const std::string_view& ciphertext)  = 0;
+    virtual std::string decrypt(const char* ciphertext, size_t size) = 0;  
 };
 
 class AES_crypt : public crypt
 {
 public:
-    virtual std::string encrypt(const std::string_view& input) override;
-    virtual std::string decrypt(const std::string_view& input) override;  
+    virtual std::string encrypt(const std::string_view& plaintext)  override;
+    virtual std::string encrypt(const char* plaintext, size_t size) override;
+
+    virtual std::string decrypt(const std::string_view& ciphertext)  override;
+    virtual std::string decrypt(const char* ciphertext, size_t size) override;
+
+private:
+    CryptoPP::AutoSeededRandomPool _rng; // random number generators
+    CryptoPP::SecByteBlock _key{CryptoPP::AES::DEFAULT_KEYLENGTH};
+    CryptoPP::SecByteBlock _iv{CryptoPP::AES::BLOCKSIZE};
 };
