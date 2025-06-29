42feca5a431316ad3a3724055fd9350681a28df0
qinchao <qinchao956@163.com>
2025-06-30 02:08:26 +0800

    优化

--- source/bee/common/event_dispatcher.h
+++ source/bee/common/event_dispatcher.h
@@ -1,20 +1,37 @@
 #pragma once
 #include <functional>
-#include <type_traits>
 #include <unordered_map>
 #include <vector>
-#include <typeindex>
 #include <memory>
 #include "lock.h"
+#include "glog.h"
 
 namespace bee
 {
+
+enum EVENT_HANDLE_RESULT : int
+{
+    EVENT_HANDLE_SUCCESS,
+    EVENT_HANDLE_INTERRUPT,
+    EVENT_HANDLE_FAILED,
+    EVENT_HANDLE_NOT_REGISTERED
+};
 
 // 事件基类
 class event_base
 {
 public:
     virtual ~event_base() = default;
+
+    // 编译期类型ID生成器
+    template <typename T>
+    static size_t type_id() noexcept
+    {
+        static char unique_id;
+        return reinterpret_cast<size_t>(&unique_id);
+    }
+
+    virtual size_t get_type_id() const noexcept = 0;
 };
 
 // 事件处理器接口
@@ -22,31 +39,30 @@ class event_handler_interface
 {
 public:
     virtual ~event_handler_interface() = default;
-    virtual void handle_event(const event_base& event) = 0;
+    virtual int handle_event(const event_base& event) = 0;
 };
 
 // 具体事件处理器
 template <typename EventType>
-requires std::is_base_of_v<event_base, EventType>
 class event_handler : public event_handler_interface
 {
 public:
-    using callback = std::function<void(const EventType&)>;
-    
+    using callback = std::function<int(const EventType&)>;
+
     explicit event_handler(callback cb) : _callback(std::move(cb)) {}
-    
-    void handle_event(const event_base& event) override
+
+    int handle_event(const event_base& event) override
     {
         if(_callback)
         {
-            // 安全类型转换
-            if(auto concrete_event = dynamic_cast<const EventType*>(&event))
+            // 安全的类型转换
+            if(event.get_type_id() == event_base::type_id<EventType>())
             {
-                _callback(*concrete_event);
+                return _callback(static_cast<const EventType&>(event));
             }
         }
+        return EVENT_HANDLE_NOT_REGISTERED;
     }
-
 private:
     callback _callback;
 };
@@ -57,35 +73,48 @@ class event_dispatcher
 public:
     // 注册事件处理器
     template <typename EventType>
-    void register_handler(std::function<void(const EventType&)> handler)
+    void register_handler(std::function<int(const EventType&)> handler)
     {
-        auto handlerPtr = std::make_shared<event_handler<EventType>>(std::move(handler));
-        
-        std::lock_guard lock(_mutex);
-        _handlers[typeid(EventType)].emplace_back(std::move(handlerPtr));
+        const size_t type_id = event_base::type_id<EventType>();
+        auto handler_ptr = std::make_unique<event_handler<EventType>>(std::move(handler));
+
+        bee::mutex::scoped l(_locker);
+        _handlers[type_id].push_back(std::move(handler_ptr));
     }
 
     // 派发事件
     void dispatch(const event_base& event)
     {
-        std::vector<std::shared_ptr<event_handler_interface>> handlers;
+        const size_t type_id = event.get_type_id();
+        std::vector<event_handler_interface*> handlers;
+
         {
-            std::lock_guard lock(_mutex);
-            if(auto iter = _handlers.find(typeid(event)); iter != _handlers.end())
+            bee::mutex::scoped l(_locker);
+            auto it = _handlers.find(type_id);
+            if(it != _handlers.end())
             {
-                handlers = iter->second;
+                handlers.reserve(it->second.size());
+                for(auto& handler : it->second)
+                {
+                    handlers.push_back(handler.get());
+                }
             }
         }
-        
-        for(auto& handler : handlers)
+
+        for(auto handler : handlers)
         {
-            handler->handle_event(event);
+            int result = handler->handle_event(event);
+            if(result != EVENT_HANDLE_SUCCESS)
+            {
+                local_log("event handle failed, type: %zu, result: %d", type_id, result);
+                if(result == EVENT_HANDLE_INTERRUPT)
+                    break;
+            }
         }
     }
-
 private:
-    std::unordered_map<std::type_index, std::vector<std::shared_ptr<event_handler_interface>>> _handlers;
-    bee::mutex _mutex;
+    std::unordered_map<size_t, std::vector<std::unique_ptr<event_handler_interface>>> _handlers;
+    bee::mutex _locker;
 };
 
 } // namespace bee
